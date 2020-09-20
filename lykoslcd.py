f0929e31b281cd3c25d9dfe59f6512bb2a66acad
joselobinho@gmail.com <joselobinho@gmail.com>
2020-09-20 00:44:47 -0300

    ajustes

--- lykoslcd.py
+++ lykoslcd.py
@@ -1,10 +1,29 @@
-from tkinter import *
+#!/usr/sbin/evn python3
+# Jose de Almeida Lobino 2020-09-19
+
+# Module Adafruit PN532 RFID/NFC
+import board
+import busio
+from digitalio import DigitalInOut
+from adafruit_pn532.spi import PN532_SPI
+
+# Module Lcd 16x2 I2c
 from libs.lcddriver import lcd
+
+from tkinter import * # GUI
 from datetime import datetime
+from threading import Thread
+from subprocess import getoutput
+from multiprocessing import Process
 import time
 
 root=Tk()
-display=lcd()
+display=lcd() # Lcd 16x2 I2c
+
+# Adafruit PN532 RFID/NFC
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+cs_pin = DigitalInOut(board.D5)
+pn532 = PN532_SPI(spi, cs_pin, debug=False)
 
 class Application:
 
@@ -12,7 +31,11 @@ class Application:
 
         self.window=root
         self.config_window()
-        self.update_clock()
+
+
+        #self.show_display()
+        #x = Thread(target=self.thread_sync_database_external, args=(1,))
+        #x.start()
         root.mainloop()
 
     def config_window(self):
@@ -24,7 +47,7 @@ class Application:
         self.window["background"]="#0C3E69"
         self.window.minsize(700,400)
 
-        self.read_card=Button(self.window,text='Simular leitura do cartão',
+        self.read_card=Button(self.window,text='Finalizar process',
                               background="#21629A",
                               foreground="#CAE6FF",
                               border="11",
@@ -32,7 +55,17 @@ class Application:
                               width=40,
                               height=5,
                               relief="groove", #Efeito { solid,sunken, etc.. ou nada }
-                              command=self.employee)
+                              command=lambda : self.finishing_process())
+
+        self.read_cards=Button(self.window,text='Iniciar processo',
+                              background="#21629A",
+                              foreground="#CAE6FF",
+                              border="11",
+                              font="Verdana 15 bold", #//Fonte, tamanho, tipo
+                              width=40,
+                              height=5,
+                              relief="groove", #Efeito { solid,sunken, etc.. ou nada }
+                              command=lambda :  self.new_process())
 
         self.press_exit=Button(self.window,text='Pressione para sair',
                               background="#21629A",
@@ -45,28 +78,99 @@ class Application:
                               command=self.exit_press)
 
         self.read_card.pack()
+        self.read_cards.pack()
         self.press_exit.pack()
 
+        #self.sql_maintenance = False
+
+        #self.thread_read_nfc=None
+        self.show_display()
+        self.thread_database_external = Process(target=self.sync_database_external)
+#        self.thread_read_nfc = Process(target=self.read_nfc)
+
+        self.thread_database_external.start()
+ #       self.thread_read_nfc.start()
+
+
+    def new_process(self):
+        self.sql_maintenance = False
+        #if not self.thread_read_nfc.is_alive():
+
+         #   del self.thread_read_nfc
+
+          #  self.thread_read_nfc = Process(target=self.read_nfc)
+          #  self.thread_read_nfc.start()
+
+    def finishing_process(self):
+        #print("Pedido de finalizacao")
+        #if self.thread_read_nfc.is_alive():
+         #   self.thread_read_nfc.terminate()
+          #  print("Finalizado")
+        self.sql_maintenance=False
+
+    def sync_database_external(self):
+
+        self.thread_read_nfc = Process(target=self.read_nfc)
+        self.thread_read_nfc.start()
+
+        while True:
+
+            saida = getoutput("ping -c 1 10.0.0.102|grep rtt |awk '{print $4}'|cut -d'/' -f2|cut -d'.' -f1")
+            if saida and int(saida) <=40:
+                print("Saida: ",saida,self.thread_database_external)
+
+                try:
+                    if self.thread_read_nfc.is_alive():
+                        self.thread_read_nfc.terminate()
+                        self.show_display(args=("EM MANUTENCAO", "A G U A R D E"), maintenance=True)
+                        print('*****************************************************************')
+
+                        time.sleep(2)
+                        self.sql_maintenance = True
+                except: pass
+            else:   print("Fora de alcance")
+
+            try:
+                if not self.thread_read_nfc.is_alive():
+                    print("/////////////////////////////////////////////")
+                    del self.thread_read_nfc
+
+                    self.thread_read_nfc = Process(target=self.read_nfc)
+                    self.thread_read_nfc.start()
+                    self.show_display()
+                    print('-----------------------------------------------------------------')
+            except:
+                self.show_display(args=("ACESSANDO", "AGUARDE..."), maintenance=True)
+
+
+        self.thread_read_nfc = Process(target=self.read_nfc)
+        self.thread_read_nfc.start()
+
     def exit_press(self):
+
+        if self.thread_database_external:  self.thread_database_external.terminate()
+        #if self.thread_read_nfc:    self.thread_read_nfc.terminate()
+        display.lcd_clear()
         self.window.destroy()
 
-    def employee(self):
+    def show_display(self,args=(None,None), maintenance=False):
 
-        self.show_display(line_2='Ola Lobinho')
-        time.sleep(1.5)
+        line_1, line_2=args
+        display.lcd_clear()
+        if line_1:  display.lcd_display_string(line_1, 1)
+        if line_2:  display.lcd_display_string(line_2, 2)
 
-    def update_clock(self):
-        self.show_display()
-        self.window.after(1000, self.update_clock)
+        if not maintenance:
+            time.sleep(1)
+            display.lcd_display_string("A P R O X I M E", 1)
+            display.lcd_display_string(" O C A R T A O", 2)
 
-    def show_display(self,line_1=None, line_2=None):
+    def read_nfc(self):
 
-        now=datetime.now().strftime(" %d/%m %H:%M:%S ")
-        if not line_1:  line_1=now
-        if not line_2:  line_2=" Passe o cartao "
-        else:   display.lcd_display_string((" "*16), 2)
+        while True:
+            pn532.SAM_configuration() # Configure PN532 to communicate with MiFare cards
+            uid = pn532.read_passive_target(timeout=0.5)
+            if uid: self.show_display(args=('Seja bem vindo','Funcionario'))
+if __name__ == '__main__':
+    Application()
 
-        display.lcd_display_string(line_1, 1) # Write line of text to first line of display
-        if line_2:  display.lcd_display_string(line_2, 2) # Write line of text to second line of display
-
-Application()
